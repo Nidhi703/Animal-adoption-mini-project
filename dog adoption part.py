391c9517b057429038f2837281d781ec8c66d0b3
Nidhi703 <105424777+Nidhi703@users.noreply.github.com>
2022-07-06 17:02:16 +0530

    Update dog adoption part.py

--- dog adoption part.py
+++ dog adoption part.py
@@ -33,25 +33,156 @@ opt.config(width=90, font=('Helvetica', 12))
 opt.pack()
 
 def openNewWindow():
+  global img2
+  global img3
+  global img4
+  global img5
+  global img6
+  global variable 
+  global variable1
+  global newWindow
 
-  global img1
   newWindow = tk.Toplevel(window)
-  newWindow.title("New page")
+  newWindow.title("Next")
+  newWindow.configure(bg = "light yellow")
   newWindow.geometry("700x400")
-  label1 = tk.Label(newWindow, text = "Trial",fg = "black", bg = "beige", font = "bold",)
-  label1.pack()
-  if variable.get() == "Beagle" and variable1.get() == "Senior":
-    label4 = tk.Label(newWindow, text = "Senior Beagle")
+  
+  tk.Button(newWindow,text="CLICK ME", command=openNewWindow2).pack(side=tk.BOTTOM)
+
+
+
+  if variable2.get() == "Dog":
+    label2 = tk.Label(newWindow, text = "Breed",font = 15)
+    label2.pack()
+    OptionList = [
+    "Beagle",
+    "Pug",
+    "German Shepherd",
+    "Labrador"
+    ] 
+    variable = tk.StringVar(window)
+    variable.set(OptionList[0])
+
+    opt = tk.OptionMenu(newWindow, variable, *OptionList)
+    opt.config(width=90, font=('Helvetica', 12))
+    opt.pack()
+
+    label3 =tk.Label(newWindow, text = "Age", font = 15)
+    label3.pack()
+    OptionList1 = [
+    "1-5",
+    "5-10",
+    "Senior",
+    ] 
+
+    variable1 = tk.StringVar(newWindow)
+    variable1.set(OptionList1[0])
+
+    opt1 = tk.OptionMenu(newWindow, variable1, *OptionList1)
+    opt1.config(width=90, font=('Helvetica', 12))
+    opt1.pack()
+
+    canvas2 = tk.Canvas(newWindow, width = 221, height = 228)  
+    canvas2.pack()  
+    img2 = ImageTk.PhotoImage(Image.open(r"download (2).jpg"))  
+
+    canvas2.create_image(1,1, anchor=tk.NW,image=img2) 
+
+
+
+  if variable2.get() == "Cat":
+    label4 = tk.Label(newWindow, text = "Breed",font = 15)
     label4.pack()
-    canvas1 = tk.Canvas(newWindow,width = 300, height = 300)  
-    canvas1.pack()  
-    img1= ImageTk.PhotoImage(Image.open(r"c:\Users\navne\Desktop\Python\cute.jpg"))  
-    canvas1.create_image(20, 20, anchor=tk.NW, image=img1) 
+    OptionList2 = [
+    "Persian",
+    "Siamese",
+    "Maine Coon",
+    "Siberian"
+    ] 
+    variable = tk.StringVar(window)
+    variable.set(OptionList2[0])
+
+    opt = tk.OptionMenu(newWindow, variable, *OptionList2)
+    opt.config(width=90, font=('Helvetica', 12))
+    opt.pack()
+
+    label5 =tk.Label(newWindow, text = "Age", font = 15)
+    label5.pack()
+    OptionList3 = [
+    "1-5",
+    "5-10",
+    "Senior",
+    ] 
+
+    variable1 = tk.StringVar(newWindow)
+    variable1.set(OptionList3[0])
+
+    opt1 = tk.OptionMenu(newWindow, variable1, *OptionList3)
+    opt1.config(width=90, font=('Helvetica', 12))
+    opt1.pack()
+
+    canvas3 = tk.Canvas(newWindow, width = 275, height = 183)  
+    canvas3.pack()  
+    img3 = ImageTk.PhotoImage(Image.open(r"cat.jpg")) 
+    canvas3.create_image(1,1, anchor=tk.NW,image=img3)
+  
+
+  if variable2.get() == "Rabbit":
+    label6 = tk.Label(newWindow, text = "Color",font = 15)
+    label6.pack()
+    OptionList4 = [
+    "White",
+    "Black",
+    "Brown"
+    ] 
+    variable = tk.StringVar(window)
+    variable.set(OptionList4[0])
+
+    opt = tk.OptionMenu(newWindow, variable, *OptionList4)
+    opt.config(width=90, font=('Helvetica', 12))
+    opt.pack()
 
 
+    canvas4 = tk.Canvas(newWindow, width = 225, height = 225)  
+    canvas4.pack()  
+    img4 = ImageTk.PhotoImage(Image.open(r"rabbit.jpg"))  
 
-button = tk.Button(window, text="Click me", command=openNewWindow)
+    canvas4.create_image(1,1, anchor=tk.NW,image=img4) 
+
+
+  if variable2.get() == "Bird":
+    label7 = tk.Label(newWindow, text = "Which bird?",font = 15)
+    label7.pack()
+    OptionList5 = [
+    "Parrot",
+    "Love birds"
+    ] 
+    variable = tk.StringVar(window)
+    variable.set(OptionList5[0])
+
+    opt = tk.OptionMenu(newWindow, variable, *OptionList5)
+    opt.config(width=90, font=('Helvetica', 12))
+    opt.pack()
+
+    canvas5 = tk.Canvas(newWindow, width = 300, height = 168)  
+    canvas5.pack()  
+    img5 = ImageTk.PhotoImage(Image.open(r"parrot.jpg"))  
+
+    canvas5.create_image(1,1, anchor=tk.NW,image=img5) 
+
+    canvas6 = tk.Canvas(newWindow, width = 300, height = 168)  
+    canvas6.pack()  
+    img6 = ImageTk.PhotoImage(Image.open(r"lovebirds.jpg"))  
+
+    canvas6.create_image(1,1, anchor=tk.NW,image=img6) 
+
+
+    
+
+    
+button = tk.Button(window, text="Next", command=openNewWindow)
 button.pack(side=tk.BOTTOM)
+
 
 
 window.mainloop()
